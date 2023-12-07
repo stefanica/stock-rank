b856599acbea6db95b52130740485d106100058a
Catalin <catalin_stefanica@yahoo.com>
2023-12-07 23:43:34 +0200

    od index HTML Form and javascript to handle the form

--- liant.js
+++ liant.js
@@ -1,2 +1,40 @@
-let parag = document.getElementById("parag")
-parag.textContent = "This is a blank template for a web page."
+const searchForm = document.getElementById('searchForm');
+let parentDiv = document.getElementById("parentDiv");
+const infoParag = document.getElementById('infoParag');
+
+ //event listner for stock search
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    infoParag.textContent = "Test 1";
+    const searchFormOBJ = new FormData(searchForm);
+    /*searchFormOBJ.append("userName", usernameCKB);
+    searchFormOBJ.append("userPass", userPassCKB);
+    searchFormOBJ.append('opcode', 'getSearchArt');*/
+    getSearchData(searchFormOBJ);
+
+    async function getSearchData(formDataSearch) {
+    const editPH = "artManPH.php";
+    const response = await fetch (
+        editPH, 
+        {
+            method: 'POST',
+            body: formDataSearch
+        });
+    const phpResponse = await response.json();
+
+   infoParag.textContent = "Search by title results: ";
+   removeChildren(parentDiv);
+    
+    //writeArt(phpResponse);
+    }
+
+});
+
+
+//removes element children
+function removeChildren(parent) {
+    while (parent.hasChildNodes()) {
+        parent.removeChild(parent.lastChild);
+      }
+}
